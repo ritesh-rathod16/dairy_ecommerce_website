4429d345fa5a9fd605716b80dc2533c4da1449e1
Ritesh Rathod <riteshrathod016@gmail.com>
2026-07-27 01:17:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,29 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
+  },
+
+  build: {
+    sourcemap: false,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+          ],
+          charts: [
+            "recharts",
+          ],
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 1000,
   },
 });
